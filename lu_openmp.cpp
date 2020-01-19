d8bacb66744db4bda39d168b761e6f5fda0b65fd
Shivam Bansal <32170952+ShivamBansal99@users.noreply.github.com>
2020-01-19 16:14:33 +0530

    vector to double pointer

--- lu_openmp.cpp
+++ lu_openmp.cpp
@@ -8,9 +8,9 @@ int main(int argc, char const *argv[])
 {
         int n, t;
         scanf("%d %d", &n, &t);
-		vector<double > a( n*n ,0 );
+		double** a = (double **)malloc(n*sizeof(double*));
 		vector<double> l( n*n ,0);
-		vector<double> u( n*n , 0);
+		double** u = (double **)malloc(n*sizeof(double*));
 		vector<vector<double> > a_copy( n , vector<double> (n, 0));
 		vector<double> pi( n);
 		//double start = time(0);
@@ -18,11 +18,14 @@ int main(int argc, char const *argv[])
 		omp_set_num_threads(t);
 		#pragma omp parallel
 		{
-			#pragma omp for collapse(2)
+			//#pragma omp for collapse(2)
 			for(int i=0; i<n; i++){
+				a[i] = (double *)malloc(n*sizeof(double));
+				u[i] = (double *)malloc(n*sizeof(double));
 				for(int j=0;j<n;j++){
-					a[change_index(i,j,n)] = drand48();
-					a_copy[i][j] = a[change_index(i,j,n)];
+					u[i][j]=0;
+					a[i][j] = drand48();
+					a_copy[i][j] = a[i][j];
 				}
 			}
 			#pragma omp for
@@ -53,9 +56,9 @@ int main(int argc, char const *argv[])
 				//#pragma omp parallel for reduction(max:max_val) reduction(=:k_1)
 				for(int i = k; i < n; i++)
                 {
-                        if(max_val < abs(a[change_index(i,k,n)]))
+                        if(max_val < abs(a[i][k]))
                         {
-                                max_val = abs(a[change_index(i,k,n)]);
+                                max_val = abs(a[i][k]);
                                 k_1 = i;
                         }
                 }
@@ -73,9 +76,9 @@ int main(int argc, char const *argv[])
                 pi[k_1] = temp;
 				#pragma omp parallel for
 				for(int i=0;i<n;i++){
-					double temp1 = a[change_index(k,i,n)];
-					a[change_index(k,i,n)]=a[change_index(k_1,i,n)];
-					a[change_index(k_1,i,n)]=temp1;
+					double temp1 = a[k][i];
+					a[k][i]=a[k_1][i];
+					a[k_1][i]=temp1;
 				}
 				#pragma omp parallel for
 				for(int i=0;i<k;i++){
@@ -83,40 +86,40 @@ int main(int argc, char const *argv[])
 					l[change_index(k,i,n)]=l[change_index(k_1,i,n)];
 					l[change_index(k_1,i,n)]=temp1;
 				}
-				u[change_index(k,k,n)]=a[change_index(k,k,n)];
+				u[k][k]=a[k][k];
 				#pragma omp parallel for
 				for (int i = k+1; i < n; i++)
                 {
-                        l[change_index(i,k,n)] = a[change_index(i,k,n)]/u[change_index(k,k,n)];
-                        u[change_index(k,i,n)] = a[change_index(k,i,n)];
+                        l[change_index(i,k,n)] = a[i][k]/u[k][k];
+                        u[k][i] = a[k][i];
 						//cout<<l[i][k]<<endl;
                 }
-				int k1 = k*n;
+				//int k1 = k*n;
 				#pragma omp distribute parallel for	simd
 				for(int i=k+1;i<n;i++){
-					int i1= i*n;
-					register double temp1 = l[i1+k];
+					// int i1= i*n;
+					register double temp1 = l[i*n+k];
 					for(int j=k+1;j<n;j++){
-						a[i1+j]-=temp1*u[k1+j];
+						a[i][j]-=temp1*u[k][j];
 					}
 				}
         }
 		//double stop = time(0);
 		auto stop = chrono::high_resolution_clock::now();
 		cout<<"time taken: "<<(double)(chrono::duration_cast<chrono::milliseconds>(stop-start).count())/1000<<endl;
-		double res =0.0;
-		vector<vector<double> > r( n , vector<double> (n, 0));
-		#pragma omp parallel for collapse(2) reduction(+:res)
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				for(int k=0;k<n;k++){
-					r[i][j]+=l[change_index(i,k,n)]*u[change_index(k,j,n)];
-				}
-				double cache = a_copy[(int)pi[i]][j] - r[i][j];
-				res+=cache*cache;
-			}
-		}
-		cout<<res<<endl;
+		// double res =0.0;
+		// vector<vector<double> > r( n , vector<double> (n, 0));
+		// #pragma omp parallel for collapse(2) reduction(+:res)
+		// for(int i=0;i<n;i++){
+			// for(int j=0;j<n;j++){
+				// for(int k=0;k<n;k++){
+					// r[i][j]+=l[change_index(i,k,n)]*u[k][j];
+				// }
+				// double cache = a_copy[(int)pi[i]][j] - r[i][j];
+				// res+=cache*cache;
+			// }
+		// }
+		// cout<<res<<endl;
 		// for(int i=0;i<n;i++){
 			// for(int j=0;j<n;j++){
 				// cout<<a_copy[(int)pi[i]][j]<<' ';
